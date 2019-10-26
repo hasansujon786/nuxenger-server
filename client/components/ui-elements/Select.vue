a8a8704c9111204c07d6ba10171ad6d7f32267b6
Hasan Mahmud <hasansujon786@gmail.com>
2019-10-26 15:44:02 -0700

    Client - create checkbox element

---
+++ client/components/ui-elements/Select.vue
@@ -0,0 +1,38 @@
+<template functional>
+  <label :class="props.classNames" class="ui-select inline-flex cursor-pointer relative">
+    <slot></slot>
+    <div
+      class="ui-select__bg w-6 h-6 bg-white rounded shadow 
+      bg-center bg-contain bg-no-repeat border border-gray-400"
+    ></div>
+    <span v-if="props.label" class="select-none ml-2">{{ props.label }}</span>
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    classNames: String,
+    label: String
+  }
+}
+</script>
+
+<style scoped>
+.ui-select input[type='checkbox']:checked + .ui-select__bg {
+  background-image: url('~assets/svg/check-white.svg');
+  background-color: #3182ce !important;
+  border-color: #3182ce !important;
+  box-shadow: 0 0 0 4px rgba(49, 130, 206, 0.3);
+}
+
+input[type='checkbox']:focus + .ui-select__bg {
+  @apply shadow-outline;
+}
+
+/* Keep it in global Style file */
+.ui-select input[type='checkbox'] {
+  all: initial;
+  opacity: 0;
+}
+</style>
--- client/pages/random.vue
+++ client/pages/random.vue
@@ -129,35 +129,15 @@
     </section>
 
     <section>
-      <h1>inputs</h1>
+      <h1 class="mb-2">inputs</h1>
       <!-- To handle checked state this component requires a single of css -->
-      <label class="custom-label flex">
-        <div class="bg-white shadow w-6 h-6 p-1 flex justify-center items-center mr-2">
-          <input type="checkbox" class="hidden" checked />
-          <svg class="hidden w-4 h-4 text-green-600 pointer-events-none" viewBox="0 0 172 172">
-            <g
-              fill="none"
-              stroke-width="none"
-              stroke-miterlimit="10"
-              font-family="none"
-              font-weight="none"
-              font-size="none"
-              text-anchor="none"
-              style="mix-blend-mode:normal"
-            >
-              <path d="M0 172V0h172v172z" />
-              <path
-                d="M145.433 37.933L64.5 118.8658 33.7337 88.0996l-10.134 10.1341L64.5 139.1341l91.067-91.067z"
-                fill="currentColor"
-                stroke-width="1"
-              />
-            </g>
-          </svg>
-        </div>
-        <span class="select-none">
-          This is a sample checkbox. All the text will toggle the state</span
-        >
-      </label>
+      <ui-select classNames="ml-2" label="Option 1">
+        <input v-model="selectValue" type="checkbox" />
+      </ui-select>
+
+      <ui-select classNames="ml-2" label="Option 2">
+        <input v-model="selectValue2" type="checkbox" />
+      </ui-select>
     </section>
 
     <section>
@@ -174,16 +154,28 @@
 </template>
 
 <script>
+import SelectVue from '../components/ui-elements/Select.vue'
 export default {
   // middleware: 'auth'
+  data() {
+    return {
+      selectValue: false,
+      selectValue2: false
+    }
+  },
+  methods: {
+    logInfo() {
+      console.log('key pressed')
+    }
+  },
+  components: {
+    uiSelect: SelectVue
+  }
 }
 </script>
 
 <style>
 /* section inputs */
-.custom-label input:checked + svg {
-  display: block !important;
-}
 section {
   margin: 3rem 1rem;
 }
