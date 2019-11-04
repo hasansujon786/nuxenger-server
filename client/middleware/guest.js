9268ff9bbd9e9596f8643cc367817be9379c9208
Hasan Mahmud <hasansujon786@gmail.com>
2019-11-04 16:06:28 +0600

    delete client

--- client/middleware/guest.js
+++
@@ -1,9 +0,0 @@
-export default ({ redirect, app: { $apolloHelpers } }) => {
-  const token = $apolloHelpers.getToken()
-  // const authUser = store.getters['auth/authUser']
-  if (token) {
-    // if no toket
-    console.info('You are already signid in.')
-    redirect('/')
-  }
-}
--- client/utils/index.js
+++
@@ -1,7 +0,0 @@
-export const scrollToBottomOfChatBox = ({ el, smooth }) => {
-  const element = document.getElementById(el)
-  // element.scrollIntoView(false)
-  smooth
-    ? element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
-    : element.scrollIntoView(false)
-}
